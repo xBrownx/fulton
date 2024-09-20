cb0d2c4ddd7b58c868880a2ad9eb6f0abfbd9930
xbrownx <asbrownn@gmail.com>
2024-09-20 18:03:02 +1000

    first commit

--- src/components/pages/design/index.jsx
+++ src/components/pages/design/index.jsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import { designConst as CONST } from './constants';
 import { Page } from '../../templates';
 import { FeaturesList } from "../../organisms/index.jsx";
@@ -6,16 +6,26 @@ import { Column, Video } from "../../atoms/index.jsx";
 import { StyledVideo, VideoContainer } from "./styles.jsx";
 
 
+
 function Design(props) {
+    const videos = CONST.assets.videos;
+    const videoSrcArray = Object.keys(videos).map(key => videos[key].src);
+    const [currentShot, setCurrentShot] = useState(0);
+    const [videoSrc, setVideoSrc] = useState(videoSrcArray[0]);
+    const [isLoading, setIsLoading] = useState(false);
+    const videoRef = useRef(null);
+
     return (
         <Page
             $bgPrimary
             $fullScreen
             $pageRef={props.$pageRef}
         >
-            <VideoContainer>
-                <StyledVideo autoPlay loop muted controls={false} {...props}>
-                    <source src={CONST.assets.videos.loadingDocks.src} type="video/mp4" />
+            <VideoContainer
+                $isLoading={isLoading}
+            >
+                <StyledVideo ref={videoRef} muted controls={false} {...props}>
+                    <source src={videoSrc} type="video/mp4" />
                 </StyledVideo>
             </VideoContainer>
             <Column $paddingTop={113}>
